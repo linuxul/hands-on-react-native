94a046e9a58e58a0a1007a673ef4abb8931c0974
linuxul <jsseo@eflash.co.kr>
2024-02-09 17:46:53 +0900

    [feat] 책 2장 rn-calc-study 프로젝트 생성 #1

---
+++ rn-calc-study/src/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const App = () => {
+    console.log('start app');
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto"/>
+            <Text>Calc App!!</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default App;
