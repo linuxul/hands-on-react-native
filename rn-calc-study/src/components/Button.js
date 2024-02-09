545bd61b8bf8db31d49bdd38bb46708466dbc025
linuxul <jsseo@eflash.co.kr>
2024-02-09 21:34:54 +0900

    [feat] 책 2장 rn-calc-study 프로젝트 생성 - Button - color #5

--- rn-calc-study/src/components/Button.js
+++ rn-calc-study/src/components/Button.js
@@ -1,85 +1,53 @@
-import { Text, Pressable } from "react-native";
+import { Text, StyleSheet, Pressable } from "react-native";
 import PropTypes from "prop-types";
 
-const Button = ({ title }) => {
+const ButtonTypes = {
+    NUMBER: 'NUMBER',
+    OPERATOR: 'OPERATOR'
+};
+
+const Button = ({ title, onPress, buttonStyle, buttonType }) => {
   console.log(title);
 
-  //   return <Text>{title}</Text>;
   return (
     <Pressable
       style={({ pressed }) => [
-        { backgroundColor: "red", padding: 20 },
-        pressed && { backgroundColor: "blue" }
+        styles.button,
+        {
+            backgroundColor: buttonType === ButtonTypes.NUMBER ? '#71717a' : '#f59e0b',
+        },
+        pressed && { backgroundColor: buttonType === ButtonTypes.NUMBER ? '#3f3f46' : '#b45309' },
+        buttonStyle
       ]}
-      onPressIn={() => console.log("onPressIn")}
-      onPressOut={() => console.log("onPressOut")}
-      onPress={() => console.log("onPress")}
-      onLongPress={() => console.log("onLongPress")}
-      delayLongPress={2000}
+      onPressOut={onPress}
     >
-      <Text style={{ color: "white" }}>{title}</Text>
+      <Text style={styles.title}>{title}</Text>
     </Pressable>
   );
 };
 
 Button.defaultProps = {
-  title: "button title"
-};
+    buttonType: ButtonTypes.NUMBER
+}
 
 Button.propTypes = {
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired,
+  buttonStyle: PropTypes.object,
+  buttonType: PropTypes.oneOf(Object.values(ButtonTypes))
 };
 
+const styles = StyleSheet.create({
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#71717a"
+  },
+  title: {
+    color: "#ffffff",
+    fontSize: 50
+  }
+});
+
+export { ButtonTypes };
 export default Button;
-
-// import { Text, TouchableHighlight } from "react-native";
-// import PropTypes from "prop-types";
-
-// const Button = ({ title }) => {
-//   console.log(title);
-
-//   //   return <Text>{title}</Text>;
-//   return (
-//     <TouchableHighlight
-//       style={{ backgroundColor: "red", padding: 20 }}
-//       underlayColor="orange"
-//       onPress={() => {}}
-//     >
-//       <Text style={{ color: "white" }}>{title}</Text>
-//     </TouchableHighlight>
-//   );
-// };
-
-// Button.defaultProps = {
-//   title: "button title"
-// };
-
-// Button.propTypes = {
-//   title: PropTypes.string.isRequired
-// };
-
-// export default Button;
-
-// import { Text, TouchableOpacity } from "react-native";
-// import PropTypes from "prop-types";
-
-// const Button = ({ title }) => {
-//   console.log(title);
-
-// //   return <Text>{title}</Text>;
-//     return (
-//         <TouchableOpacity style={{ backgroundColor: 'red', padding: 20 }}>
-//             <Text style={{ color: 'white'}}>{title}</Text>
-//         </TouchableOpacity>
-//     );
-// };
-
-// Button.defaultProps = {
-//   title: "button title"
-// };
-
-// Button.propTypes = {
-//   title: PropTypes.string.isRequired
-// };
-
-// export default Button;
