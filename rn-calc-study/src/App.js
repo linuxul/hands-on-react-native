a18fde593e6d1af68e2dd1d45c5c45661919e677
linuxul <jsseo@eflash.co.kr>
2024-02-09 19:26:11 +0900

    [feat] 책 2장 rn-calc-study 프로젝트 생성 - Button #3

--- rn-calc-study/src/App.js
+++ rn-calc-study/src/App.js
@@ -1,44 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Button from "./components/Button";
 
 const App = () => {
-  console.log('start app');
-  console.log(true && 'React Native');
-  console.log(false && 'React Native');
-  console.log('React Native' && true);
-  console.log('React Native' && false);
-  console.log('jsseo' && 'React Native');
-  console.log('' && 'React Native');
-  console.log('React Native' && {});
-  console.log({} && 'React Native');
-  console.log(null && 'React Native');
-  console.log(0 && 'React Native');
-
-  const isError = false;
+  console.log("###### start app ######");
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text style={styles.text}>Calc App</Text>
-      <Text style={styles.text}>StyleSheet</Text>
-      <Text style={styles.error}>Error</Text>
-      <Text style={[styles.text, styles.error]}>Style Text, Error</Text>
-      <Text style={[styles.error, styles.text]}>Style Text, Error</Text>
-      <Text style={[styles.text, isError && styles.error]}>Error && 연산자</Text>
-      {/* <Text
-        style={{
-          fontSize: 30,
-          fontWeight: '700',
-          color: 'green',
-          backgroundColor: 'yellow',
-          borderWidth: 1,
-          borderColor: 'blue',
-          paddingHorizontal: 20,
-          paddingVertical: 10,
-        }}
-      >
-        Calc App!!
-      </Text> */}
+      {/* <Button title="button" onPress={() => console.log('Click!!')} style={{ backgroundColor: 'black', color: 'white' }} /> */}
+      {/* <Button title="button" onPress={() => console.log('Click!!')} color="purple" backgroundColor="blue" /> */}
+      {/* <Button title="button" onPress={() => console.log('Click!!')} color="red" /> */}
+      {/* <MyButton>My Button</MyButton> */}
+      {/* <Button title="My Button#">My Button</Button> */}
+      <Button onPress={() => console.log("click!")} color="purple"></Button>
+      <Button title="title" />
     </View>
   );
 };
@@ -46,24 +23,24 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
   text: {
     fontSize: 30,
-    fontWeight: '700',
-    color: 'green',
-    backgroundColor: 'yellow',
+    fontWeight: "700",
+    color: "green",
+    backgroundColor: "yellow",
     borderWidth: 1,
-    borderColor: 'blue',
+    borderColor: "blue",
     paddingHorizontal: 20,
-    paddingVertical: 10,
+    paddingVertical: 10
   },
   error: {
     fontSize: 30,
-    fontWeight: '700',
-    color: 'red',
+    fontWeight: "700",
+    color: "red"
   }
 });
 
---
+++ rn-calc-study/src/components/Button.js
@@ -0,0 +1,80 @@
+import { Text, Pressable } from "react-native";
+import PropTypes from "prop-types";
+
+const Button = ({ title }) => {
+  console.log(title);
+
+  //   return <Text>{title}</Text>;
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        { backgroundColor: "red", padding: 20 },
+        pressed && { backgroundColor: "blue" }
+      ]}
+    >
+      <Text style={{ color: "white" }}>{title}</Text>
+    </Pressable>
+  );
+};
+
+Button.defaultProps = {
+  title: "button title"
+};
+
+Button.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+export default Button;
+
+// import { Text, TouchableHighlight } from "react-native";
+// import PropTypes from "prop-types";
+
+// const Button = ({ title }) => {
+//   console.log(title);
+
+//   //   return <Text>{title}</Text>;
+//   return (
+//     <TouchableHighlight
+//       style={{ backgroundColor: "red", padding: 20 }}
+//       underlayColor="orange"
+//       onPress={() => {}}
+//     >
+//       <Text style={{ color: "white" }}>{title}</Text>
+//     </TouchableHighlight>
+//   );
+// };
+
+// Button.defaultProps = {
+//   title: "button title"
+// };
+
+// Button.propTypes = {
+//   title: PropTypes.string.isRequired
+// };
+
+// export default Button;
+
+// import { Text, TouchableOpacity } from "react-native";
+// import PropTypes from "prop-types";
+
+// const Button = ({ title }) => {
+//   console.log(title);
+
+// //   return <Text>{title}</Text>;
+//     return (
+//         <TouchableOpacity style={{ backgroundColor: 'red', padding: 20 }}>
+//             <Text style={{ color: 'white'}}>{title}</Text>
+//         </TouchableOpacity>
+//     );
+// };
+
+// Button.defaultProps = {
+//   title: "button title"
+// };
+
+// Button.propTypes = {
+//   title: PropTypes.string.isRequired
+// };
+
+// export default Button;
