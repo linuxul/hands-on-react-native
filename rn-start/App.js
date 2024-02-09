ad7070547125c5aa96d92bccf1ab6ee9482117db
linuxul <jsseo@eflash.co.kr>
2024-02-09 11:58:16 +0900

    [feat] 책 1장 prettier & eslint 설정

--- rn-start/App.js
+++ rn-start/App.js
@@ -2,9 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('Expo React Native.')
+
+  const name = 'tom';
+  const isFullname = true;
+
+  const add = (a, b) => {
+    return a + b;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View 
+    style={styles.container}
+    // 여는 태그 주석
+    >
+      <Text>My First React Native!</Text>
+      <Text>My name is {name}</Text>
+      <Text>1 + 2 = {add(1, 2)}</Text>
+      <Text>{isFullname === true ? name + ' kim' : name}</Text>
       <StatusBar style="auto" />
     </View>
   );
