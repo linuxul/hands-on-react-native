1fc20dc9ec9b15d60fb27bae06041891399c7a8d
linuxul <jsseo@eflash.co.kr>
2024-02-14 11:23:22 +0900

    [feat] 책 7장 rn-photo-sutdy splash 적용 #7

--- rn-photo-study/src/navigations/Navigation.js
+++ rn-photo-study/src/navigations/Navigation.js
@@ -1,9 +1,41 @@
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './AuthStack';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import { Asset } from 'expo-asset';
 
 const Navigation = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Asset.fromModule(
+          require('../../assets/cover.png')
+        ).downloadAsync();
+      } catch (error) {
+        console.log('error : ' + error);
+      } finally {
+        setIsReady(true);
+      }
+    })();
+  }, []);
+
+  const onReady = async () => {
+    console.log('onReady');
+
+    if (isReady) {
+      await SplashScreen.hideAsync();
+    }
+  };
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={onReady}>
       <AuthStack></AuthStack>
     </NavigationContainer>
   );
