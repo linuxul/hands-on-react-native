9fa2a77dab032eb3f6c7c37d2df82bf1220dc3e7
linuxul <jsseo@eflash.co.kr>
2024-02-15 09:57:32 +0900

    [feat] 책 7장 rn-photo-sutdy 홈화면 만들기 #19

--- rn-photo-study/src/screens/HomeScreen.js
+++ rn-photo-study/src/screens/HomeScreen.js
@@ -1,18 +1,52 @@
 import { useNavigation } from '@react-navigation/native';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import {
+  Image,
+  Pressable,
+  useWindowDimensions,
+  Button,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 import { WHITE } from '../colors';
 import { MainRoutes } from '../navigations/routes';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { ContentRoutes } from '../navigations/routes';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const { top } = useSafeAreaInsets();
+  const height = useWindowDimensions().height / 4;
 
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-      <Button
-        title="Select"
-        onPress={() => navigation.navigate(MainRoutes.SELECT_PHOTOS)}
-      ></Button>
+    <View style={[styles.container, { paddingTop: top }]}>
+      <View style={styles.topContainer}>
+        <Image
+          source={require('../../assets/icon.png')}
+          style={styles.icon}
+        ></Image>
+        <Text style={styles.title}>PlacePhotos</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable onPress={() => navigation.navigate(ContentRoutes.LIST)}>
+          { console.log('height : ' + height)}
+          <Image
+            source={require('../../assets/home-clock.png')}
+            style={[styles.image, { height }]}
+          ></Image>
+          <Text style={styles.buttonTitle}>타임라인</Text>
+        </Pressable>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable onPress={() => navigation.navigate(ContentRoutes.MAP)}>
+          { console.log('height : ' + height)}
+          <Image
+            source={require('../../assets/home-map.png')}
+            style={[styles.image, { height }]}
+          ></Image>
+          <Text style={styles.buttonTitle}>지도</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -22,7 +56,39 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: WHITE
+    backgroundColor: WHITE,
+    paddingHorizontal: 20
+  },
+  topContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20
+  },
+  icon: {
+    width: 60,
+    height: 60,
+    borderRadius: 30
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: '700',
+    marginLeft: 10
+  },
+  buttonContainer: {
+    marginVertical: 20
+  },
+  image: {
+    borderRadius: 10,
+    // width: '100%'
+  },
+  buttonTitle: {
+    position: 'absolute',
+    color: WHITE,
+    fontSize: 40,
+    fontWeight: '700',
+    bottom: 30,
+    left: 30
   }
 });
 
