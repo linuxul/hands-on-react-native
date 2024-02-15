655b5d18b58573ff2a86864f8e36c252f262b443
linuxul <jsseo@eflash.co.kr>
2024-02-15 17:14:25 +0900

    [feat] 책 9장 rn-photo-sutdy image picker #23

--- rn-photo-study/src/screens/ImagePickerScreen.js
+++ rn-photo-study/src/screens/ImagePickerScreen.js
@@ -1,45 +1,74 @@
 import { useNavigation } from '@react-navigation/native';
-import { useEffect, useLayoutEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect, useLayoutEffect, useState } from 'react';
 import HeaderRight from '../components/HeaderRight';
 import * as MediaLibrary from 'expo-media-library';
+import {
+  Alert,
+  StyleSheet,
+  View,
+  FlatList,
+  Image,
+  Pressable,
+  useWindowDimensions
+} from 'react-native';
 
 const ImagePickerScreen = () => {
   const navigation = useNavigation();
   const [status, requestPermission] = MediaLibrary.usePermissions();
 
-  // const getPhotos = async() => {
-  //   const options = {
-  //     first: 30,
-  //     sortBy: [MediaLibrary.SortBy.creationTime]
-  //   }
-  //   const res = await MediaLibrary.getAssetsAsync(options)
-  //   console.log('res : ' + res.assets)
-  //   console.log('image : ' + res.endCursor + ', next : ' + res.hasNextPage + ', count : ' + res.totalCount)
+  const width = useWindowDimensions().width / 3;
+  const [photos, setPhotos] = useState([]);
+  const [listInfo, setListInfo] = useState({
+    endCursor: '',
+    hasNextPage: true
+  });
 
-  // }
+  const getPhotos = async () => {
+    const options = {
+      first: 30,
+      sortBy: [MediaLibrary.SortBy.creationTime]
+    };
 
-  // useEffect(() => {
-  //   if (status?.granted) {
-  //     getPhotos()
-  //   }
-  // }, [status?.granted])
+    if (listInfo.hasNextPage) {
+      const { assets, endCursor, hasNextPage } =
+        await MediaLibrary.getAssetsAsync(options);
+      setPhotos((prev) => [...prev, ...assets]);
+      setListInfo({ endCursor, hasNextPage });
+    }
+
+    // const res = await MediaLibrary.getAssetsAsync(options);
+    // console.log('res : ' + res.assets);
+    // console.log(
+    //   'image : ' +
+    //     res.endCursor +
+    //     ', next : ' +
+    //     res.hasNextPage +
+    //     ', count : ' +
+    //     res.totalCount
+    // );
+  };
 
   useEffect(() => {
-    (async () => {
-      const { granted } = await requestPermission();
-      if (!granted) {
-        Alert.alert('사진 접근 권한', '사진 접근 권한이 필요합니다.', [
-          {
-            text: '확인',
-            onPress: () => {
-              navigation.canGoBack && navigation.goBack();
-            }
-          }
-        ]);
-      }
-    })();
-  }, [navigation, requestPermission]);
+    if (status?.granted) {
+      getPhotos();
+    }
+  }, [status?.granted]);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const { granted } = await requestPermission();
+  //     if (!granted) {
+  //       Alert.alert('사진 접근 권한', '사진 접근 권한이 필요합니다.', [
+  //         {
+  //           text: '확인',
+  //           onPress: () => {
+  //             navigation.canGoBack && navigation.goBack();
+  //           }
+  //         }
+  //       ]);
+  //     }
+  //   })();
+  // }, [navigation, requestPermission]);
   console.log('status : ' + status);
 
   useLayoutEffect(() => {
@@ -50,7 +79,15 @@ const ImagePickerScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Image Picker</Text>
+      <FlatList
+        style={styles.list}
+        data={photos}
+        renderItem={({ item }) => (
+          <Pressable style={{ width, height: width }}>
+            <Image source={{ uri: item.uri }} style={styles.photo}></Image>
+          </Pressable>
+        )}
+      ></FlatList>
     </View>
   );
 };
@@ -60,6 +97,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  list: {
+    width: '100%'
+  },
+  photo: {
+    width: '100%',
+    height: '100%'
   }
 });
 
