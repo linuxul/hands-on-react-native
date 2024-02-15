893c3d4a45dcebe1678d5b462d69d898d839a579
linuxul <jsseo@eflash.co.kr>
2024-02-15 20:08:34 +0900

    [feat] 책 9장 rn-photo-sutdy image picker 소스 정리 #26

--- rn-photo-study/src/screens/ImagePickerScreen.js
+++ rn-photo-study/src/screens/ImagePickerScreen.js
@@ -1,77 +1,25 @@
-import { useNavigation, useNavigationState } from '@react-navigation/native';
 import {
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useState,
-  useRef
-} from 'react';
+  useNavigation,
+  useNavigationState,
+  useRoute
+} from '@react-navigation/native';
+import { useCallback, useLayoutEffect, useState } from 'react';
 import HeaderRight from '../components/HeaderRight';
-import * as MediaLibrary from 'expo-media-library';
-import {
-  Alert,
-  StyleSheet,
-  View,
-  FlatList,
-  Image,
-  Pressable,
-  useWindowDimensions,
-  Platform
-} from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { PRIMARY } from '../colors';
-import { BlurView } from 'expo-blur';
-
-const initialListInfo = { endCursor: '', hasNextPage: true };
+import ImagePicker from '../components/ImagePicker';
 
 const ImagePickerScreen = () => {
   const navigation = useNavigation();
-  const [status, requestPermission] = MediaLibrary.usePermissions();
-
-  const width = useWindowDimensions().width / 3;
-  const [photos, setPhotos] = useState([]);
-  const listInfo = useRef(initialListInfo);
-  const [refreshing, setRefreshing] = useState(false);
-  const [selectedPhotos, setSelectedPhotos] = useState([]);
   const stateRoutes = useNavigationState((state) => state.routes);
-  // console.log('state : ' + JSON.stringify(state))
+
+  const { params } = useRoute();
+
+  const maxCount = params?.maxCount ?? 1;
+  const [selectedPhotos, setSelectedPhotos] = useState([]);
 
   const onSelect = useCallback(() => {
     const prevScreenName = stateRoutes[stateRoutes.length - 2].name;
     navigation.navigate(prevScreenName, { selectedPhotos });
   }, [navigation, selectedPhotos, stateRoutes]);
-
-  const getPhotos = useCallback(async () => {
-    const options = {
-      first: 30,
-      sortBy: [MediaLibrary.SortBy.creationTime]
-    };
-
-    if (listInfo.current.endCursor) {
-      options['after'] = listInfo.current.endCursor;
-    }
-
-    if (listInfo.current.hasNextPage) {
-      const { assets, endCursor, hasNextPage } =
-        await MediaLibrary.getAssetsAsync(options);
-      setPhotos((prev) => (options.after ? [...prev, ...assets] : assets));
-      listInfo.current = { endCursor, hasNextPage };
-    }
-  }, []);
-  console.log('photo : ' + photos.length);
-
-  const onRefresh = async () => {
-    setRefreshing(true);
-    listInfo.current = initialListInfo;
-    await getPhotos();
-    setRefreshing(false);
-  };
-
-  useEffect(() => {
-    if (status?.granted) {
-      getPhotos();
-    }
-  }, [getPhotos, status?.granted]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -90,68 +38,25 @@ const ImagePickerScreen = () => {
 
   const togglePhoto = (photo) => {
     const isSelected = isSelectedPhoto(photo);
-    setSelectedPhotos((prev) =>
-      isSelected
-        ? prev.filter((item) => item.id !== photo.id)
-        : [...prev, photo]
-    );
+    setSelectedPhotos((prev) => {
+      if (isSelected) {
+        return prev.filter((item) => item.id !== photo.id);
+      }
+
+      if (maxCount > prev?.length) {
+        return [...prev, photo];
+      }
+
+      return prev;
+    });
   };
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        style={styles.list}
-        data={photos}
-        renderItem={({ item }) => {
-          const isSelected = isSelectedPhoto(item);
-          return (
-            <Pressable
-              style={{ width, height: width }}
-              onPress={() => togglePhoto(item)}
-            >
-              <Image source={{ uri: item.uri }} style={styles.photo}></Image>
-              {isSelected && (
-                <BlurView
-                  style={[StyleSheet.absoluteFill, styles.checkIcon]}
-                  intensity={Platform.select({ ios: 10, android: 50 })}
-                >
-                  <MaterialCommunityIcons
-                    name="check-circle"
-                    size={40}
-                    color={PRIMARY.DEFAULT}
-                  ></MaterialCommunityIcons>
-                </BlurView>
-              )}
-            </Pressable>
-          );
-        }}
-        numColumns={3}
-        onEndReached={getPhotos}
-        onEndReachedThreshold={0.4}
-        onRefresh={onRefresh}
-        refreshing={refreshing}
-      ></FlatList>
-    </View>
+    <ImagePicker
+      togglePhoto={ togglePhoto }
+      isSelectedPhoto={isSelectedPhoto}
+    ></ImagePicker>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  list: {
-    width: '100%'
-  },
-  photo: {
-    width: '100%',
-    height: '100%'
-  },
-  checkIcon: {
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
 
 export default ImagePickerScreen;
